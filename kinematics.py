612f6b85673b9650e3db64b6fcaeeb5a12686448
jjcheung <94059335+jjcheung@users.noreply.github.com>
2022-08-24 11:24:22 +0800

    Update kinematics.py

--- kinematics.py
+++ kinematics.py
@@ -1,2 +1,6 @@
-# You are in a basketball court, where the basketball rim is 3 metres above the ground.
-# Write a boolean function that takes in the horizontal distance between you and the basketball rim, the angle and the speed at which you throw the ball, and return whether the ball will reach the rim.
+"""
+You are in a basketball court, where the basketball rim is 3 metres above the ground.
+
+Write a boolean function that takes in the horizontal distance between you and the basketball rim, 
+the angle above the ground and the speed at which you throw the ball, and return whether the ball will reach the rim.
+"""
